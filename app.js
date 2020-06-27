631d284346e62ac0fbfc0f2e7e17dbbc40d00e63
Andrew <andrew.gaynor@ymail.com>
2020-06-27 17:38:13 -0400

    add signup page and signup form

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => {
-    res.render('index')
-})
+    res.render('index');
+});
+
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
